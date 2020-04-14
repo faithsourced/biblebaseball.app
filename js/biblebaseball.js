e3a3c583125d37b17d84585bad45c36279523c59
Mark Jenkinson <mark@togglemedia.com>
2020-04-14 16:21:16 -0400

    pitch speed difficulty settings, ie11 compat

--- js/biblebaseball.js
+++ js/biblebaseball.js
@@ -1,6 +1,7 @@
 var question_packs = [];
 var questions = [];
 var asked = [];
+var pitch = [];
 var inning = 1;
 var half = 'bottom';
 var outs = 0;
@@ -12,31 +13,61 @@ var first_base = 0;
 var second_base = 0;
 var third_base = 0;
 
+var pitch_speed = 0;
+var this_countdown = 0;
+var pitch_stopwatch;
+var countdown_timer = '<div id="pitch_countdown"><span class="number"></span><svg><circle r="18" cx="20" cy="20"></circle></svg></div>';
+
 $(document).ready(function() {
 	$('.mound').on('click', '.pitcher', function() {
 		if($(".pitcher").css('opacity') == 1) { // prevent double clicks
-			pitch();
+			throwPitch();
 		}
-	});
-	
-	$('.out').on('click', function() {
-		out();
-	});
-	
-	$('.hit').on('click', function() {
-		hit();
 	});
 	
 	$('.answers').on('click', '.answer', function() {
-		answer($(this).attr('data-question'), $(this).attr('data-choice'));
-	});
-	
-	$('.mound').on('change', '.question_pack_selector', function() {
-		if($(this).val()) {
-			loadQuestions($(this).val());
+		if($(this).css('opacity') == 1) { // only register if opacity is 1, otherwise, time has expired
+			answer($(this).attr('data-question'), $(this).attr('data-choice'));
 		}
 	});
+
+	$('.mound').on('click', '.play_ball', function() {
+		var question_pack = $('#question_pack_selector').val();
+		pitch_speed = $('#pitch_speed_selector').val();
+		this_countdown = pitch_speed;
+		
+		if(question_pack) {
+			loadQuestions(question_pack);
+		}
+	});	
 });
+
+function countdown() {
+	if(pitch_speed > 0) {
+		if(this_countdown == pitch_speed) { // setup
+			$(".mound").html(countdown_timer);
+			$("#pitch_countdown").fadeIn(300);
+			$('#pitch_countdown svg circle').css({'animation' : 'countdown '+pitch_speed+'s linear 1 forwards'});
+		}
+		if (this_countdown == 0) {
+			out();
+		} else {
+			$('#pitch_countdown .number').html(this_countdown);
+			this_countdown--;
+		}
+	}
+	else {
+		clearTimeout(pitch_stopwatch);
+	}
+}
+
+function resetCountdown() {
+	clearTimeout(pitch_stopwatch);
+	this_countdown = pitch_speed;
+	$('#pitch_countdown').fadeOut(300, function() {
+		$(".mound").empty();
+	});
+}
 
 function loadChoices(question, choices) {
 	choices = shuffleChoices(choices);
@@ -45,25 +76,27 @@ function loadChoices(question, choices) {
 		var choice_html = "<button class=\"answer\" data-question=\""+question+"\" data-choice=\""+choice.id+"\">"+choice.text+"</button>";
 		$(".answers").append(choice_html);
 	});
-	$(".answers").fadeIn();
+	$(".answers").fadeIn(300);
 }
 
-function answer(question, choice) {
-	if(questions[question]["answer"][0].id == choice) {
+function answer(question_id, choice_id) {
+	if(questions[question_id]["answer"][0].id == choice_id) {
 		hit();
 	}
 	else {
-		$('.playbyplay').html("<p>Out!<br />"+questions[question]["answer"][0].explanation+"</p>");
 		out();
 	}
+	$('.answers').empty();
 }
 
-function pitch() {
-	hidePitcher();
-	var pitch = pickQuestion();
-	setQuestion(pitch.question);
-	showQuestion();
-	loadChoices(pitch.id, pitch.choices);
+function throwPitch() {
+	removePitcher();
+	$('.playbyplay').fadeOut(300, function() {
+		pitch = pickQuestion();
+		setQuestion();
+		showQuestion();
+		pitch_stopwatch = setInterval(countdown, 1000);
+	});
 }
 
 function pickQuestion() {
@@ -71,7 +104,7 @@ function pickQuestion() {
 		asked = [];	
 	}
 	var pick = questions[Math.floor(Math.random()*questions.length)];
-	if (!asked.includes(pick.id)) {
+	if (!includes(asked, pick.id)) {
 		asked.push(pick.id);
 		return pick;
 	}
@@ -83,56 +116,98 @@ function pickQuestion() {
 function loadQuestions(file) {
 	$.getJSON('questions/'+file, function(data) {
 		questions = data;
-		$(".question_pack_selector").fadeOut(100, function() {
+		$(".mound_visit").fadeOut(300, function() {
 			addPitcher();
-			showPitcher();
-			$(".logo").fadeOut();
 		});
+		$(".logo").fadeOut(300);
    });
+}
+
+function sortQuestionPacks(element, prop, propType, asc) {
+  switch (propType) {
+    case "int":
+      element = element.sort(function (a, b) {
+        if (asc) {
+          return (parseInt(a[prop]) > parseInt(b[prop])) ? 1 : ((parseInt(a[prop]) < parseInt(b[prop])) ? -1 : 0);
+        } else {
+          return (parseInt(b[prop]) > parseInt(a[prop])) ? 1 : ((parseInt(b[prop]) < parseInt(a[prop])) ? -1 : 0);
+        }
+      });
+      break;
+    default:
+      element = element.sort(function (a, b) {
+        if (asc) {
+          return (a[prop].toLowerCase() > b[prop].toLowerCase()) ? 1 : ((a[prop].toLowerCase() < b[prop].toLowerCase()) ? -1 : 0);
+        } else {
+          return (b[prop].toLowerCase() > a[prop].toLowerCase()) ? 1 : ((b[prop].toLowerCase() < a[prop].toLowerCase()) ? -1 : 0);
+        }
+      });
+  }
+}
+
+function includes(data, value) {
+	var pos = data.indexOf(value);
+	if (pos >= 0) {
+		return true;
+	}
+	return false;
 }
 
 function loadQuestionPacks() {
 	$.getJSON('questions/packs.json', function(data) {
+		sortQuestionPacks(data , "id", "int", false);
 		question_packs = data;
-		var question_pack_selector = "<select id=\"question_pack_selector\" class=\"question_pack_selector\">";
-		question_pack_selector += "<option value=\"\">Choose your Bible Baseball Trivia&hellip;</option>";
+		
+		var mound_visit = "<div class=\"mound_visit\"><select id=\"question_pack_selector\" name=\"question_pack\">";
+			mound_visit += "<option value=\"\">Trivia Pack&hellip;</option>";
 		
 		$.each(question_packs, function (index, question_pack) {
-			question_pack_selector += "<option value=\""+question_pack.file+"\">"+question_pack.name+" ("+question_pack.difficulty+")</option>";
+			mound_visit += "<option value=\""+question_pack.file+"\">"+question_pack.name+" ("+question_pack.difficulty+")</option>";
 		});
 		
-		question_pack_selector += "</select>";
+			mound_visit += "</select><br />";
 		
-		$(".mound").html(question_pack_selector);
+			mound_visit += "<select id=\"pitch_speed_selector\" name=\"pitch_speed\">";
+			mound_visit += "<option value=\"\">Pitch Speed&hellip;</option>";
+			mound_visit += "<option value=\"0\" selected>Little League</option>";
+			mound_visit += "<option value=\"15\">Rookie Ball</option>";
+			mound_visit += "<option value=\"10\">Minor League</option>";
+			mound_visit += "<option value=\"5\">Major League</option>";
+			mound_visit += "<option value=\"3\">Hall of Fame</option>";
+			mound_visit += "</select><br />";
+			
+			mound_visit += "<button class=\"btn play_ball\">Play Ball</button></div>";
+			
+		$(".mound").html(mound_visit);
    });
 }
 
 function addPitcher() {
-	$(".mound").html("<button class=\"pitcher\">Pitch</button>");
+	$(".mound").html("<button class=\"btn pitcher\">Pitch</button>");
+	$(".pitcher").fadeIn(300);
 }
 
-function showPitcher() {
-	$(".pitcher").fadeIn();
-}
-
-function hidePitcher() {
-	$(".pitcher").fadeOut();
+function removePitcher() {
+	$(".pitcher").fadeOut(300, function() {
+		$(".mound").empty();
+	});
 }
 
 function showRunners() {
 	$('.runner').hide();
 	if(first_base == 1) {
-		$('.runner.first').fadeIn();
+		$('.runner.first').fadeIn(300);
 	}
 	if(second_base == 1) {
-		$('.runner.second').fadeIn();
+		$('.runner.second').fadeIn(300);
 	}
 	if(third_base == 1) {
-		$('.runner.third').fadeIn();
+		$('.runner.third').fadeIn(300);
 	}
 }
 
 function hit() {
+	resetCountdown();
 	hideQuestion();
 	hideChoices();
 	var hit_text = "";
@@ -155,21 +230,47 @@ function hit() {
 		hitSingle();
 	}
 	
-	$('.playbyplay').each(function () {
-		var $this = $(this);
-		
-		$this.html("<p>Hit! "+hit_text+"</p>").fadeIn();
-		if (playbyplay_delay) {
-			window.clearTimeout(playbyplay_delay);
-		}
-		playbyplay_delay = window.setTimeout(function() {
-			$this.fadeOut(100, function() {
-				showPitcher();
-			});
-		}, 3000);
-	});
+	if (playbyplay_delay) {
+		window.clearTimeout(playbyplay_delay);
+	}
+	playbyplay_delay = window.setTimeout(function() {
+		addPitcher();
+	}, 2000);
+	
+	$('.playbyplay').html("<p>Hit! "+hit_text+"</p>").fadeIn(300);
 	
 	showRunners();
+}
+
+function out() {
+	resetCountdown();
+	hideQuestion();
+	hideChoices();
+	$('.playbyplay').html("<p>Out!<br />"+questions[pitch.id]["answer"][0].explanation+"</p>").fadeIn(300);
+	
+	$('.outs h1').each(function () {
+		var $this = $(this);
+		var outs = parseInt($(this).html(), 10);
+		++outs;
+		$this.html(outs);
+		
+		if(outs == 3) {
+			if (change_inning_delay) {
+				window.clearTimeout(change_inning_delay);
+			}
+			change_inning_delay = window.setTimeout(function() {
+				$this.html(0);
+				changeInning();
+			}, 1000);
+		}
+	});
+	
+	if (playbyplay_delay) {
+		window.clearTimeout(playbyplay_delay);
+	}
+	playbyplay_delay = window.setTimeout(function() {
+		addPitcher();
+	}, 2000);
 }
 
 function hitHomerun() {
@@ -253,16 +354,18 @@ function increaseScore() {
 	});
 }
 
-function setQuestion(question) {
-	$('.question').html('<p>' + question + '</p>');
+function setQuestion() {
+	$('.question').html('<p>' + pitch.question + '</p>');
 }
 
 function showQuestion() {
-	$(".question").fadeIn();
+	$(".question").fadeIn(300, function() {
+		loadChoices(pitch.id, pitch.choices);
+	});
 }
 
 function hideQuestion() {
-	$(".question").fadeOut();
+	$(".question").fadeOut(300);
 }
 
 function shuffleChoices(choices) {
@@ -285,42 +388,7 @@ function shuffleChoices(choices) {
 }
 
 function hideChoices() {
-	$(".answer").fadeOut();
-}
-
-function out() {
-	hideQuestion();
-	hideChoices();
-	$('.outs h1').each(function () {
-		var $this = $(this);
-		var outs = parseInt($(this).html(), 10);
-		++outs;
-		$this.html(outs);
-		
-		if(outs == 3) {
-			if (change_inning_delay) {
-				window.clearTimeout(change_inning_delay);
-			}
-			change_inning_delay = window.setTimeout(function() {
-				$this.html(0);
-				changeInning();
-			}, 1000);
-		}
-	});
-	
-	$('.playbyplay').each(function () {
-		var $this = $(this);
-		
-		$this.fadeIn();
-		if (playbyplay_delay) {
-			window.clearTimeout(playbyplay_delay);
-		}
-		playbyplay_delay = window.setTimeout(function() {
-			$this.fadeOut(100, function() {
-				showPitcher();
-			});
-		}, 2000);
-	});
+	$(".answer").fadeOut(300);
 }
 
 function changeInning() {
